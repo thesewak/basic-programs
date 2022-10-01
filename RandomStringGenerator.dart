a5f9f812abaf2f42fdb12680d51822d4f6699ee9
The Sewak <46748623+thesewak@users.noreply.github.com>
2022-10-01 13:49:18 +0545

    Code Changes

--- RandomStringGenerator.dart
+++ RandomStringGenerator.dart
@@ -9,6 +9,3 @@ void main() {
 
 const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
 Random _rnd = Random();
-
-String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
